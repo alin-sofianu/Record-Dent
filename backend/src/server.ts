db554e3c6bf297bdf8b05f380409c3d6633d0d4f
alin-sofianu <alin.sofianu@gmail.com>
2024-01-21 18:34:08 +0200

    Configure MongoDB Atlas and connect the app to it, use .env, envalidate package and mongoose.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,21 @@
+import "dotenv/config" //place this line very first
+import mongoose from "mongoose"
+import env from "./utils/validateEnv"
+
 import express from "express";
 const app = express();
-const port = undefined;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Hello, world")
 })
 
-app.listen(port!, () => {
-    console.log("Server is listening on port: " + port)
-})
+const port = env.PORT
+
+mongoose.connect(env.MONGO_CONNECTION_STRING)
+    .then(() => {
+        console.log("Connected to the DB")
+        app.listen(port, () => {
+            console.log("Server is listening on port: " + port)
+        })
+    })
+    .catch(console.error)
---
+++ backend/src/utils/validateEnv.ts
@@ -0,0 +1,9 @@
+// this file is used to make sure all the env variables are present, and not udnefined, because sometimes that could cause harm
+
+import { cleanEnv } from "envalid"
+import { str, port } from "envalid/dist/validators"
+
+export default cleanEnv(process.env, {
+    MONGO_CONNECTION_STRING: str(),
+    PORT: port()
+})
