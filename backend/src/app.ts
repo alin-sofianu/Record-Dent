db7592bf8689b13fabd97a831581e4b57d670348
alin-sofianu <alin.sofianu@gmail.com>
2024-01-25 14:48:17 +0200

    Setup MongoDB model for notes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import "dotenv/config" //place this line very first
 import express from "express";
+// this is a default export so it can be named anything here
+import NoteModel from "./models/note"
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello, world")
+app.get('/', async (req, res) => {
+    // .exec() is used just because we don't get a promise returned, because we used async await
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
 })
 
 export default app
