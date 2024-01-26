45508d24efc17b7effd6beb7bb53b0530a0d1d51
alin-sofianu <alin.sofianu@gmail.com>
2024-01-26 16:42:00 +0200

    Add Note comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Note } from "./models/note";
+import { Note as NoteModel } from "./models/note";
+import Note from './components/Notes';
 
 function App() {
-  const [notes, setNotes] = useState<Note[]>([])
+  const [notes, setNotes] = useState<NoteModel[]>([])
 
   useEffect(() => {
     async function loadNotes() {
@@ -21,8 +21,10 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      {JSON.stringify(notes)}
+    <div>
+      {notes.map((note) =>
+        <Note note={note} key={note._id} />
+      )}
     </div>
   );
 }
