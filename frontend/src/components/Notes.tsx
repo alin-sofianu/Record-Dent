2e717a5568e937d50b178495272d4a1a25082301
alin-sofianu <alin.sofianu@gmail.com>
2024-01-26 17:27:21 +0200

    Add css modules files and styles

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -1,20 +1,30 @@
 import styles from "../styles/Note.module.css"
 import { Card } from "react-bootstrap"
 import { Note as NoteModel } from "../models/note"
+import { formatDate } from "../utils/formatDates";
 
 interface NoteProps {
-    note: NoteModel
+    note: NoteModel,
+    className?: string,
 }
 
-const Note = ({ note }: NoteProps) => {
+const Note = ({ note, className }: NoteProps) => {
     const { title, text, createdAt, updatedAt } = note;
 
+    let createdUpdatedText: string;
+    if (updatedAt > createdAt) {
+        createdUpdatedText = "Updated at: " + formatDate(updatedAt)
+    } else {
+        createdUpdatedText = "Created at: " + formatDate(createdAt)
+    }
+
     return (
-        <Card className={styles.noteCard}>
-            <Card.Body>
+        <Card className={`${styles.noteCard} ${className}`}>
+            <Card.Body className={styles.cardBody}>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text className={styles.cardText}>{text}</Card.Text>
             </Card.Body>
+            <Card.Footer>{createdUpdatedText}</Card.Footer>
         </Card>
     )
 }
